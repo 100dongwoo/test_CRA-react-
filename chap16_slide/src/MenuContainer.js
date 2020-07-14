10fa64d8b6ff3ab1191fed1f8172912149032712
dongwoo <qorehddn123@naver.com>
2020-07-15 01:19:33 +0900

    ch16_slide_clear

--- chap16_slide/src/MenuContainer.js
+++ chap16_slide/src/MenuContainer.js
@@ -1,9 +1,36 @@
-import React,{Component}from "react"
+import React, {Component} from "react"
+import MenuButton from "./MenuButton";
+import Menu from "./Menu"
+class MenuContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            visible: false
+        }
+        this.toggleMenu = this.toggleMenu.bind(this);
+        this.handleMouseDown=this.handleMouseDown.bind(this)
+    }
 
-class MenuContainer extends Component{
+
+    handleMouseDown(e){
+        this.toggleMenu();
+        console.log("clicked")
+        e.stopPropagation();
+    }
+
+
+    toggleMenu() {
+        this.setState({
+            visible: !this.state.visible
+        })
+    }
+
     render() {
-        return(
+        return (
             <div>
+                <MenuButton handleMouseDown={this.handleMouseDown}/>
+                <Menu handleMouseDown={this.handleMouseDown}
+                      menuvisibility={this.state.visible}/>
                 <div>
                     <p> Can you spot the item that doesn't belong</p>
                     <ul>
